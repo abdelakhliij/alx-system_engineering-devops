a5b366f009fc9db299985f5759a151a3e24eba80
abdelakhliij <riadakhlij@gmail.com>
2025-01-10 12:00:00 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,13 @@
 #Install an especific version of flask (2.1.0)
 
-package { 'python3,8':
+package { 'python3.8':
   ensure   => '3.8.10',
   provider => 'pip3',
 }
 
 package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
 
 package { 'werkzeug':
